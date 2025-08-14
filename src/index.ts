600891d391b09d893eb1f9f3c89db502b0ae69a6
heryrmdn <heryrhamadan@gmail.com>
2025-08-14 11:47:25 +0700

    add: feature pagination

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,15 @@ import { getUserEvents } from "./utils";
 const program = new Command();
 
 program
-  .version("1.0.0")
   .description("Simple CLI to fetch the recent activity of a GitHub user")
+  .version("1.0.0")
   .argument("<username>", "username")
-  .action(async (username) => await getUserEvents(username));
+  .option("--perpage <number>", "custom total items per page (default: 20)")
+  .option("--page <number>", "go to specify page (default: 1)")
+  .action(async (username, options) => {
+    let perpage = options.perpage ? options.perpage : 20;
+    let page = options.page ? options.page : 1;
+    await getUserEvents(username, perpage, page);
+  });
 
 program.parse(process.argv);
